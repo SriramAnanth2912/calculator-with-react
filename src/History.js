2a73551fcfc6bd8354b5e7125fc64299a90fdf61
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-04-10 12:52:43 +0530

    Improved passing args to History

--- src/History.js
+++ src/History.js
@@ -2,20 +2,29 @@ import React from "react";
 
 function History({ history }) {
   let date = new Date();
-  let dateTime = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
-  let dateDate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  let dateTime = `${date.getHours()}:${date.getMinutes()}`;
+  let fullDate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
   return (
     <>
-      <h2>{dateDate}</h2>
-      <ul>
-        {history.map((calculation, index) => (
-          <li key={index}>
-            <span className="time">Time:{dateTime}</span>
-            <br></br>
-            {calculation}
-          </li>
-        ))}
-      </ul>
+      <h2>{fullDate}</h2>
+      {history.length > 0 ? (
+        <ul>
+          {history.map((calculation, index) =>
+            history[index].num1 !== "" ? (
+              <li key={index}>
+                <span className="time">Time:: {dateTime}</span>
+                <br></br>
+                {calculation.num1} {calculation.operator} {calculation.num2}
+                {calculation.result.toString().length > 6
+                  ? " = " + calculation.result.toString()
+                  : " = " + calculation.result.toString().slice(0, 6)}
+              </li>
+            ) : null
+          )}
+        </ul>
+      ) : (
+        ""
+      )}
     </>
   );
 }
