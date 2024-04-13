9885cb813b51e239a2d89ba2d18e94b9a91fdb41
SriramAnanth2912 <sriramananth2912@gmail.com>
2024-04-13 15:08:19 +0530

    modified date and time format

--- src/History.js
+++ src/History.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function History({ history }) {
   let date = new Date();
-  let dateTime = `${date.getHours()}:${date.getMinutes()}`;
-  let fullDate = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  let dateTime = `${
+    date.getHours() > 12
+      ? String(date.getHours() - 12).padStart(2, "0")
+      : String(date.getHours()).padStart(2, "0")
+  }:${String(date.getMinutes()).padStart(2, "0")} ${date.getHours() > 12 ? "pm" : "am"}`;
+
+  let fullDate = `${date.toLocaleString("default", {
+    month: "long",
+  })} ${String(date.getDate()).padStart(2, "0")},${date.getFullYear()}`;
   return (
     <div className="history-text">
       <h2>{fullDate}</h2>
@@ -12,7 +19,7 @@ function History({ history }) {
           {history.map((calculation, index) =>
             history[index].result !== " " || undefined ? (
               <li key={index}>
-                <span className="time">Time:: {dateTime}</span>
+                <span className="time">@ {dateTime}</span>
                 <br></br>
                 {calculation.num1} {calculation.operator} {calculation.num2}
                 {calculation.result !== undefined ? " = " + calculation.result : " "}
